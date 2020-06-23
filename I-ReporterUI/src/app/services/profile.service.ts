6a9c92683ce87d695d629f0c129aaa8f473b32e2
victormongare1 <vicmongz254@gmail.com>
2020-06-23 11:16:07 +0300

    defined a method to get a single profile in the profile service

--- I-ReporterUI/src/app/services/profile.service.ts
+++ I-ReporterUI/src/app/services/profile.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const AllProfilesUrl='http://127.0.0.1:8000/api/profiles';
+const SingleProfileUrl='http://127.0.0.1:8000/api/profile';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProfileService {
 
   constructor(private http: HttpClient) { }
-  get(id)
+
+  get(id){
+    return this.http.get(`${SingleProfileUrl}/${id}`);
+  }
 }
