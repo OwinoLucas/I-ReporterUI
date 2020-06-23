12f4d6a837994209f615de7139de2cd4eb4d0437
victormongare1 <vicmongz254@gmail.com>
2020-06-23 15:22:11 +0300

    defined methods for obtaining a single profile and updating a single profile in currentprofile component

--- I-ReporterUI/src/app/components/currentprofile/currentprofile.component.ts
+++ I-ReporterUI/src/app/components/currentprofile/currentprofile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService} from 'src/app/services/profile.service';
-
+import { ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-currentprofile',
@@ -9,9 +9,36 @@ import { ProfileService} from 'src/app/services/profile.service';
 })
 export class CurrentprofileComponent implements OnInit {
 
-  constructor() { }
+  currentProfile= null;
+  message="";
 
-  ngOnInit(): void {
+  constructor(private profileService : ProfileService,
+              private route : ActivatedRoute,
+              private router : Router) { }
+
+
+  ngOnInit() {
+    this.getProfile(this.route.snapshot.paramMap.get('id'));
   }
 
+  getProfile(id){
+    this.profileService.get(id).subscribe(data => {
+      this.currentProfile=data;
+      console.log(data);
+    },
+    error => {
+      console.log(error);
+    })
+  }
+
+  updateProfile(){
+    this.profileService.update(this.currentProfile.id,this.currentProfile).subscribe(
+      response => {
+        console.log(response);
+        this.message = 'Your profile was updated successfully'
+      },
+      error => {
+        console.log(error);
+    });
+  }
 }
