8136c3ff1a049d72b9ce733ff65129de7ba2809e
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-06-24 09:05:31 +0300

    Configured leaflet to work.Added markers to the componnet

--- I-ReporterUI/src/app/app.module.ts
+++ I-ReporterUI/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
+import { GoogleMapsModule } from '@angular/google-maps'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { AddInterventionRecordComponent } from './components/add-intervention-re
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
