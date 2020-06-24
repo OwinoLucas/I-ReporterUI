e67a1ca2dc266d68923471e1eedfe4299a72caa5
Kvria <trishakuria@gmail.com>
2020-06-24 08:10:37 +0300

    initial commit

---
+++ src/app/authentication/signup/signup.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { 
+  Validators, 
+  FormArray,
+  FormGroup,
+  FormControl,
+  FormBuilder
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  newUserForm: FormGroup;
+  
+  
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+   this.newUserForm = this.formBuilder.group({
+      firstName: ['', [
+                        Validators.required, 
+                        Validators.maxLength(32),
+                        Validators.nullValidator
+                      ]
+                 ],
+      lastName: ['', Validators.maxLength(32)],
+      userName: ['', [
+                        Validators.required, 
+                        Validators.maxLength(32),
+                        Validators.nullValidator,
+                     ]
+                ],
+      email: ['', [Validators.required,
+                  Validators.nullValidator,
+                  Validators.email
+                  ]
+              ],
+      password: ['', [Validators.required,
+                      Validators.nullValidator,
+                      Validators.minLength(8),
+                    ]]
+   })
+  }
+
+}
