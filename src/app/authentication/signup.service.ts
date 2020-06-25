114c0e12e36eee89e3585d650885905e55f237be
Kvria <trishakuria@gmail.com>
2020-06-25 14:35:42 +0300

    Create newUser function to post the newUserDetails.

--- src/app/authentication/signup.service.ts
+++ src/app/authentication/signup.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
+  httpheaders = new HttpHeaders({ 'Content-Type':'application/json' })
+  constructor( private httpclient : HttpClient ){ }
 
-  constructor() { }
+
+  createNewUser( newUserDetails : {}): Observable<{}>{
+    let signupAPI:string = 'http://127.0.0.1:8000/signup/';
+    return this.httpclient.post(
+      signupAPI,
+      newUserDetails,
+      {headers: this.httpheaders}
+    )
+  }
+
 }
