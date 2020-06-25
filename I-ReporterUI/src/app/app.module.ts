5294f283fce6509a2c0cfc4da1d667928f17e8bd
Dan Njoroge <greatdaniels@gmail.com>
2020-06-25 23:04:32 +0300

    create login component

--- I-ReporterUI/src/app/app.module.ts
+++ I-ReporterUI/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
