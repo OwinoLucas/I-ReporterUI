a22e768c5cb3d6f73ea187c7d0f69a7318990563
Kvria <trishakuria@gmail.com>
2020-06-26 07:39:14 +0300

    Map HomeComponent to a path.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './authentication/signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
