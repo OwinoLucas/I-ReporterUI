6d0a8c7a126af374f62422fed2d66361c4380dc7
Dan Njoroge <greatdaniels@gmail.com>
2020-06-26 09:02:31 +0300

    adding app-routing logic

--- I-ReporterUI/src/app/app-routing.module.ts
+++ I-ReporterUI/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo:"/login", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
