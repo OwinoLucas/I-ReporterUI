323473fb78bc32c8b27e0871ee233720b8fc8d65
Dan Njoroge <greatdaniels@gmail.com>
2020-06-26 08:58:02 +0300

    adding login logic, form, and styles

--- I-ReporterUI/src/app/components/login/login.component.ts
+++ I-ReporterUI/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
+import { LoginService } from './../../services/login.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private loginService: LoginService, private toastr: ToastrService) { }
+
+  showSuccess() {
+    this.toastr.success('Login Successful', 'Login');
   }
 
+  showError() {
+    this.toastr.error('Invalid Email or Password!', 'Login');
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.formGroup = new FormGroup({
+      email: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required])
+    })
+  }
+
+  loginProcess() {
+    if(this.formGroup.valid) {
+      this.loginService.login(this.formGroup.value).subscribe(result => {
+        if(result.success) {
+          console.log(result);
+          // alert(result.msg);
+          this.showSuccess();
+
+        }else {
+          alert(result.msg);
+        }},
+        error =>  {
+          console.log(error.error)
+          // alert(error.error.msg);
+          this.showError();
+        }
+      )
+    }
+  }
+
+  get email() {
+    return this.formGroup.get('email');
+  }
+
+  get password() {
+    return this.formGroup.get('password');
+  }
 }
