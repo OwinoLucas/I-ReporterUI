2c8728d7313953bb09ba745ff02fcf710ae6d2f3
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-06-29 23:09:45 +0300

    Passed token to the url so as to gain server

--- I-ReporterUI/src/app/services/interventionrecord.service.ts
+++ I-ReporterUI/src/app/services/interventionrecord.service.ts
@@ -14,23 +14,23 @@ export class InterventionRecordService {
   constructor(private http:HttpClient) { }
 
   create(data){
-    return this.http.post(`${baseUrl}api/intervention-record/create/`,data)
+    return this.http.post(`${baseUrl}api/intervention-record/create/`,data,{headers:this.headers})
   }
   getAll(){
     return this.http.get(`${baseUrl}api/intervention-records/`,{headers:this.headers});
   }
   get(id){
-    return this.http.get(`${baseUrl}api/intervention-record/detail/${id}`);
+    return this.http.get(`${baseUrl}api/intervention-record/detail/${id}`,{headers:this.headers});
   }
   delete(id){
-    return this.http.delete(`${baseUrl}api/intervention-record/detail/${id}`)
+    return this.http.delete(`${baseUrl}api/intervention-record/detail/${id}`,{headers:this.headers})
   }
   putrecord(id,data){
-    return this.http.put(`${baseUrl}api/intervention-record/detail/${id}/`,data)
+    return this.http.put(`${baseUrl}api/intervention-record/detail/${id}`,data,{headers:this.headers})
   }
   getbytitle(title){
-    this.searchdata= this.http.get(`${baseUrl}api/intervention-record/search/${title}`).subscribe(data=>{this.searchdata=data});
+    this.searchdata= this.http.get(`${baseUrl}api/intervention-record/search/${title}`,{headers:this.headers}).subscribe(data=>{this.searchdata=data});
     console.log(this.searchdata)
-    return this.http.get(`${baseUrl}api/intervention-record/search/${title}`);
+    return this.http.get(`${baseUrl}api/intervention-record/search/${title}`,{headers:this.headers});
   }
 }
