11cc15cbe8ac82512429769650b1494d21c34b73
moselamar <44118179+moselamar@users.noreply.github.com>
2020-06-29 12:37:16 +0300

    update flagdetailcomponent

--- I-ReporterUI/src/app/components/flagdetail/flagdetail.component.ts
+++ I-ReporterUI/src/app/components/flagdetail/flagdetail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FlagapiService } from 'src/app/services/flagapi.service';
+import {ActivatedRoute,Route, Router} from '@angular/router';
 
 @Component({
   selector: 'app-flagdetail',
@@ -6,10 +8,126 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flagdetail.component.css']
 })
 export class FlagdetailComponent implements OnInit {
-
-  constructor() { }
+  flag_record=null;
+  edittitle:'';
+  editdescription:'';
+  //editimage:File;
+  //editvideos:File;
+  editlatitude:number;
+  editlongitude:number;
+  submitted=false;
+  constructor(
+    private flagapiservice:FlagapiService,
+    private route:ActivatedRoute,
+    private router:Router,
+    //private toastr:ToastrService
+    ) { }
 
   ngOnInit(): void {
+    this.getFlag(this.route.snapshot.paramMap.get('id'));
+    //this.get_status(this.route.snapshot.paramMap.get('id'))
   }
+  locationmark=true;;
+  onChoseLocation(event){
+    this.editlatitude=event.coords.lat;
+    this.editlongitude=event.coords.lng;
+    this.locationmark=true;
+
+    console.log (this.editlongitude,this.editlongitude)
+  }
+    getFlag(id){
+      this.flagapiservice.get(id)
+      .subscribe(
+        data=>{
+          this.editlatitude=parseInt( data['latitude'])
+          this.editlongitude=parseInt( data['longitude'])
+          console.log(this.editlongitude)
+          this.flag_record=data;
+          console.log(data);
+        },
+        error=>{
+          console.log(error);
+        });
+    }
+
+
+    confirmdelete(event){
+      var r=confirm('Are you sure you want  to delete?');
+      if (r ==true){
+        this.deleteFlag()
+        //this.toastr.success('Deleted successfully!','Deleted successfully!')
+      }
+      else{
+        alert('nothing done');
+      }
+    }
+    showSuccess() {
+      //this.toastr.success('Nothing deleted', 'Nothing deleted');
+    }
+   /*
+    onIMageChanged(event:any){
+      this.editimage=event.target.files[0];
+    }
+    onVideoChanged(event:any){
+      this.editvideos=event.target.files[0];
+    }
+*/
+    updaterecord(){
+      const uploadData=new FormData()
+      if (this.edittitle){
+        uploadData.append('title',this.edittitle);
+      }
+      if(this.editdescription){
+        uploadData.append('description',this.editdescription);
+      }
+      /*
+      if(this.editimage){
+        uploadData.append('image',this.editimage,this.editimage.name)
+      }
+      // uploadData.append('status',this.status)
+      if(this.editvideos){
+        uploadData.append('videos',this.editvideos,this.editvideos.name)
+      }
+      */
+      if(this.editlatitude && this.editlongitude){
+        uploadData.append('latitude',this.editlatitude.toString())
+        uploadData.append('longitude',this.editlongitude.toString())
+      }
+      console.log(uploadData)
+      // const data= new FormData()
+      // const edittitle=this.intervention_record.title
+      // const editdescription=this.intervention_record.description
+      // const editstatus=this.intervention_record.status;
+      // const editimage:File=this.intervention_record.image;
+
+      // const editvideos:File=this.intervention_record.videos;
+      // const editlatitude=this.intervention_record.latitude;
+      // const editlongitude=
+
+  // longitude: 37.075142 ;
+      this.flagapiservice.putrecord(this.flag_record.id,uploadData)
+      .subscribe(
+        data=>{
+          console.log(data)
+          alert(' Flag Edited succesfully!')
+
+
+        },
+        error=>{
+          console.log(error.error)
+        }
+      )
+    }
+    deleteFlag(){
+      this.flagapiservice.delete(this.flag_record.id)
+      .subscribe(
+        data=>{
+          console.log(data);
+          this.router.navigate(['/flags/all']);
+        },
+        error=>{
+          console.log(error)
+        });
+    }
 
 }
