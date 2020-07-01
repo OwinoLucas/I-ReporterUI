3e5ff19915709cc7a0e2c0d079b539c7e9e6b764
mosesbabu <44118179+mosesbabu@users.noreply.github.com>
2020-07-01 11:30:53 +0300

    update flag api service

--- I-ReporterUI/src/app/services/flagapi.service.ts
+++ I-ReporterUI/src/app/services/flagapi.service.ts
@@ -1,28 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
-const baseUrl='http://127.0.0.1:8000/api/';
+const baseUrl='http://127.0.0.1:8000/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlagapiService {
+  searchdata:any
+  token=JSON.parse(localStorage.getItem('token'));
+  headers=new HttpHeaders().set("Authorization",`Bearer ${this.token}` )
 
   constructor(private http:HttpClient) { }
 
   create(data){
-    return this.http.post(`${baseUrl}flags/create/`,data)
+    return this.http.post(`${baseUrl}api/flags/create/`,data,{headers:this.headers})
   }
   getAll(){
-    return this.http.get(`${baseUrl}flags/`)
+    return this.http.get(`${baseUrl}api/flags/`,{headers:this.headers})
   }
   get(id){
-    return this.http.get(`${baseUrl}/${id}`);
+    return this.http.get(`${baseUrl}api/flag/detail/${id}`,{headers:this.headers});
   }
   delete(id){
-    return this.http.delete(`${baseUrl}/${id}`)
+    return this.http.delete(`${baseUrl}api/flag/detail${id}`,{headers:this.headers})
   }
   putrecord(id,data){
-    return this.http.put(`${baseUrl}/${id}/`,data)
+    return this.http.put(`${baseUrl}/${id}api/flag/detail${id}`,data,{headers:this.headers})
   }
 }
