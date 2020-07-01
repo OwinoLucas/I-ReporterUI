1ed9351d2ac256cd02cf4da105a797674cdbeb51
mosesbabu <44118179+mosesbabu@users.noreply.github.com>
2020-07-01 12:21:33 +0300

    update flagapi service

--- I-ReporterUI/src/app/services/flagapi.service.ts
+++ I-ReporterUI/src/app/services/flagapi.service.ts
@@ -14,18 +14,18 @@ export class FlagapiService {
   constructor(private http:HttpClient) { }
 
   create(data){
-    return this.http.post(`${baseUrl}api/flags/create/`,data,{headers:this.headers})
+    return this.http.post(`${baseUrl}api/flags/create/`,data,{headers:this.headers});
   }
   getAll(){
-    return this.http.get(`${baseUrl}api/flags/`,{headers:this.headers})
+    return this.http.get(`${baseUrl}api/flags/`,{headers:this.headers});
   }
   get(id){
     return this.http.get(`${baseUrl}api/flag/detail/${id}`,{headers:this.headers});
   }
   delete(id){
-    return this.http.delete(`${baseUrl}api/flag/detail${id}`,{headers:this.headers})
+    return this.http.delete(`${baseUrl}api/flag/detail${id}`,{headers:this.headers});
   }
   putrecord(id,data){
-    return this.http.put(`${baseUrl}/${id}api/flag/detail${id}`,data,{headers:this.headers})
+    return this.http.put(`${baseUrl}/${id}api/flag/detail${id}`,data,{headers:this.headers});
   }
 }
