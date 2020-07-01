1ee1f3edaf60226443d38f78150d255b5b0c5d30
Lucas Owino <lucowish35@gmail.com>
2020-07-01 12:26:03 +0300

    add new data

--- I-ReporterUI/src/app/components/flag/flag.component.ts
+++ I-ReporterUI/src/app/components/flag/flag.component.ts
@@ -1,16 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FlagapiService } from 'src/app/services/flagapi.service';
-import{ActivatedRoute,Router }from '@angular/router'
-
-@Component({
-  selector: 'app-flag',
-  templateUrl: './flag.component.html',
-  styleUrls: ['./flag.component.css']
-})
-export class FlagComponent implements OnInit {
-import { Component, OnInit } from '@angular/core';
-import { FlagapiService } from 'src/app/services/flagapi.service';
-import{ActivatedRoute,Router }from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-flag',
@@ -19,169 +9,86 @@ import{ActivatedRoute,Router }from '@angular/router'
 })
 export class FlagComponent implements OnInit {
 
-  title:'';
-  description:'';
-  image:File;
-  videos:File;
-  latitude:any ;
-  longitude:any ;
+  title: '';
+  description: '';
+  image: File;
+  videos: File;
+  latitude: any;
+  longitude: any;
 
 
 
-  onIMageChanged(event:any){
-    this.image=event.target.files[0];
+  onIMageChanged(event: any) {
+    this.image = event.target.files[0];
   }
-  onVideoChanged(event:any){
-    this.videos=event.target.files[0];
-  }
-
-  locationmark=false;
-  onChoseLocation(event){
-    this.latitude=event.coords.lat;
-    this.longitude=event.coords.lng;
-    this.locationmark=true;
-
-    console.log (this.longitude,this.longitude)
+  onVideoChanged(event: any) {
+    this.videos = event.target.files[0];
   }
 
-  submitted=false;
+  locationmark = false;
+  onChoseLocation(event) {
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+    this.locationmark = true;
+
+    console.log(this.longitude, this.longitude)
+  }
+
+  submitted = false;
 
   constructor(
-    private flagapiService:FlagapiService,
-    private router:Router,
-    ) {
-    if (navigator)
-    {
-      navigator.geolocation.getCurrentPosition(pos =>{
-        this.longitude= +pos.coords.longitude;
+    private flagapiService: FlagapiService,
+    private router: Router,
+  ) {
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.longitude = +pos.coords.longitude;
         this.latitude = +pos.coords.latitude
       });
-    }}
+    }
+  }
 
   ngOnInit(): void {
 
   }
 
-  saveFlag(){
+  saveFlag() {
 
-    const uploadData=new FormData()
-    if (this.title){
-      uploadData.append('title',this.title);
+    const uploadData = new FormData()
+    if (this.title) {
+      uploadData.append('title', this.title);
     }
-    if(this.description){
-      uploadData.append('description',this.description);
+    if (this.description) {
+      uploadData.append('description', this.description);
     }
 
-    if(this.image){
-      uploadData.append('image',this.image,this.image.name)
+    if (this.image) {
+      uploadData.append('image', this.image, this.image.name)
     }
     // uploadData.append('status',this.status)
-    if(this.videos){
-      uploadData.append('videos',this.videos,this.videos.name)
+    if (this.videos) {
+      uploadData.append('videos', this.videos, this.videos.name)
     }
 
-    if(this.latitude & this.longitude){
-      uploadData.append('latitude',this.latitude.toString())
-      uploadData.append('longitude',this.longitude.toString())
+    if (this.latitude & this.longitude) {
+      uploadData.append('latitude', this.latitude.toString())
+      uploadData.append('longitude', this.longitude.toString())
     }
     console.log(uploadData.getAll)
     this.flagapiService.create(uploadData)
 
-    .subscribe(
-      response => {
-        console.log(response);
-        this.router.navigate(['/flags/all'])
-        this.submitted = true;
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/flags/all'])
+          this.submitted = true;
 
-      },
-      error => {
-        console.log(error);
-      });
+        },
+        error => {
+          console.log(error);
+        });
 
   }
 
 }
 
-  title:'';
-  description:'';
-
-  //image:File;
-  //videos:File;
-  latitude:any ;
-  longitude:any ;
-
-  status=null;
-  /*
-  onIMageChanged(event:any){
-    this.image=event.target.files[0];
-  }
-  onVideoChanged(event:any){
-    this.videos=event.target.files[0];
-  }
-  */
-  locationmark=false;
-  onChoseLocation(event){
-    this.latitude=event.coords.lat;
-    this.longitude=event.coords.lng;
-    this.locationmark=true;
-
-    console.log (this.longitude,this.longitude)
-  }
-
-  submitted=false;
-
-  constructor(
-    private flagapiService:FlagapiService,
-    private router:Router,
-    ) {
-    if (navigator)
-    {
-      navigator.geolocation.getCurrentPosition(pos =>{
-        this.longitude= +pos.coords.longitude;
-        this.latitude = +pos.coords.latitude
-      });
-    }}
-
-  ngOnInit(): void {
-
-  }
-
-  saveFlag(){
-
-    const uploadData=new FormData()
-    if (this.title){
-      uploadData.append('title',this.title);
-    }
-    if(this.description){
-      uploadData.append('description',this.description);
-    }
-    /*
-    if(this.image){
-      uploadData.append('image',this.image,this.image.name)
-    }
-    // uploadData.append('status',this.status)
-    if(this.videos){
-      uploadData.append('videos',this.videos,this.videos.name)
-    }
-    */
-    if(this.latitude & this.longitude){
-      uploadData.append('latitude',this.latitude.toString())
-      uploadData.append('longitude',this.longitude.toString())
-    }
-    console.log(uploadData.getAll)
-    this.flagapiService.create(uploadData)
-
-    .subscribe(
-      response => {
-        console.log(response);
-        this.router.navigate(['/flags/all'])
-        this.submitted = true;
-
-      },
-      error => {
-        console.log(error);
-      });
-
-  }
-
-}
--- I-ReporterUI/src/app/components/intervention-record-details/intervention-record-details.component.ts
+++ I-ReporterUI/src/app/components/intervention-record-details/intervention-record-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { InterventionRecordService} from 'src/app/services/interventionrecord.service';
-import {ActivatedRoute,Route, Router} from '@angular/router';
-import {ToastrService} from 'ngx-toastr';
+import { InterventionRecordService } from 'src/app/services/interventionrecord.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-intervention-record-details',
@@ -9,148 +9,148 @@ import {ToastrService} from 'ngx-toastr';
   styleUrls: ['./intervention-record-details.component.css']
 })
 export class InterventionRecordDetailsComponent implements OnInit {
-  intervention_record=null;
-  edittitle:'';
-  editdescription:'';
-  editimage:File;
-  editvideos:File;
-  editlatitude:number;
-  editlongitude:number;
-  submitted=false;
+  intervention_record = null;
+  edittitle: '';
+  editdescription: '';
+  editimage: File;
+  editvideos: File;
+  editlatitude: number;
+  editlongitude: number;
+  submitted = false;
   constructor(
-    private interventionrecordservice:InterventionRecordService,
-    private route:ActivatedRoute,
-    private router:Router,
-    private toastr:ToastrService
-    ) { }
+    private interventionrecordservice: InterventionRecordService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
     this.getIntervention(this.route.snapshot.paramMap.get('id'));
     this.get_status(this.route.snapshot.paramMap.get('id'))
   }
-  locationmark=true;;
-  onChoseLocation(event){
-    this.editlatitude=event.coords.lat;
-    this.editlongitude=event.coords.lng;
-    this.locationmark=true;
-    
-    console.log (this.editlongitude,this.editlongitude)
+  locationmark = true;;
+  onChoseLocation(event) {
+    this.editlatitude = event.coords.lat;
+    this.editlongitude = event.coords.lng;
+    this.locationmark = true;
+
+    console.log(this.editlongitude, this.editlongitude)
   }
-    getIntervention(id){
-      this.interventionrecordservice.get(id)
+  getIntervention(id) {
+    this.interventionrecordservice.get(id)
       .subscribe(
-        data=>{
-          this.editlatitude=parseInt( data['latitude'])
-          this.editlongitude=parseInt( data['longitude'])
-          this.edittitle=data['title']
+        data => {
+          this.editlatitude = parseFloat(data['latitude'])
+          this.editlongitude = parseFloat(data['longitude'])
+          this.edittitle = data['title']
           console.log(this.editlongitude)
-          this.intervention_record=data;
+          this.intervention_record = data;
           console.log(data);
         },
-        error=>{
+        error => {
           console.log(error);
         });
-    }
-    nullstatus=true
-    upstatus:''
-    status:any;
-    investigation:any;
-    resolved:any;
-    rejected:any;
-    stat:any;
+  }
+  nullstatus = true
+  upstatus: ''
+  status: any;
+  investigation: any;
+  resolved: any;
+  rejected: any;
+  stat: any;
 
-    get_status(id){
-      this.interventionrecordservice.get(id)
+  get_status(id) {
+    this.interventionrecordservice.get(id)
       .subscribe(
-        data=>{
-         this.status=data['status']
-          
-         console.log(status)
-         if (status=='waiting'){
-           
-         }if (this.status=="Under Investigation"){
-           this.investigation=this.status
-           this.nullstatus=false;
-           console.log(this.investigation)
-         }else if(this.status=="rejected"){
-           this.rejected=this.status
-           this.nullstatus=false;
-         }else if (this.status=="resolved"){
-           this.resolved=this.status
-           this.nullstatus=false;
-         }else if(this.status=="waiting"){
-           this.stat=this.status
-         }
+        data => {
+          this.status = data['status']
+
+          console.log(status)
+          if (status == 'waiting') {
+
+          } if (this.status == "Under Investigation") {
+            this.investigation = this.status
+            this.nullstatus = false;
+            console.log(this.investigation)
+          } else if (this.status == "rejected") {
+            this.rejected = this.status
+            this.nullstatus = false;
+          } else if (this.status == "resolved") {
+            this.resolved = this.status
+            this.nullstatus = false;
+          } else if (this.status == "waiting") {
+            this.stat = this.status
+          }
         }
       )
-    }
+  }
 
 
 
-    confirmdelete(event){
-      var r=confirm('Are you sure to delete?');
-      if (r ==true){
-        this.deleteIntervention()
-        this.toastr.success('Deleted successfully!','Deleted successfully!')
-      }
-      else{
-        alert('nothing done');
-      }
+  confirmdelete(event) {
+    var r = confirm('Are you sure to delete?');
+    if (r == true) {
+      this.deleteIntervention()
+      this.toastr.success('Deleted successfully!', 'Deleted successfully!')
     }
-    showSuccess() {
-      this.toastr.success('Nothing deleted', 'Nothing deleted');
+    else {
+      alert('nothing done');
     }
-  
-    onIMageChanged(event:any){
-      this.editimage=event.target.files[0];
+  }
+  showSuccess() {
+    this.toastr.success('Nothing deleted', 'Nothing deleted');
+  }
+
+  onIMageChanged(event: any) {
+    this.editimage = event.target.files[0];
+  }
+  onVideoChanged(event: any) {
+    this.editvideos = event.target.files[0];
+  }
+
+  updaterecord() {
+    const uploadData = new FormData()
+    if (this.edittitle) {
+      uploadData.append('title', this.edittitle);
     }
-    onVideoChanged(event:any){
-      this.editvideos=event.target.files[0];
+    if (this.editdescription) {
+      uploadData.append('description', this.editdescription);
     }
-    
-    updaterecord(){
-      const uploadData=new FormData()
-      if (this.edittitle){
-        uploadData.append('title',this.edittitle);
-      }
-      if(this.editdescription){
-        uploadData.append('description',this.editdescription);
-      }
-      if(this.editimage){
-        uploadData.append('image',this.editimage,this.editimage.name)
-      }
-      // uploadData.append('status',this.status)
-      if(this.editvideos){
-        uploadData.append('videos',this.editvideos,this.editvideos.name)
-      }
-      if(this.editlatitude && this.editlongitude){
-        uploadData.append('latitude',this.editlatitude.toString())
-        uploadData.append('longitude',this.editlongitude.toString())
-      }
-      console.log(uploadData)
-      this.interventionrecordservice.putrecord(this.intervention_record.id,uploadData)
+    if (this.editimage) {
+      uploadData.append('image', this.editimage, this.editimage.name)
+    }
+    // uploadData.append('status',this.status)
+    if (this.editvideos) {
+      uploadData.append('videos', this.editvideos, this.editvideos.name)
+    }
+    if (this.editlatitude && this.editlongitude) {
+      uploadData.append('latitude', this.editlatitude.toString())
+      uploadData.append('longitude', this.editlongitude.toString())
+    }
+    console.log(uploadData)
+    this.interventionrecordservice.putrecord(this.intervention_record.id, uploadData)
       .subscribe(
-        data=>{
+        data => {
           console.log(data)
           alert('Editted succesfully!')
           this.getIntervention(this.route.snapshot.paramMap.get('id'));
 
         },
-        error=>{
+        error => {
           console.log(error.error)
         }
       )
-    }
-    deleteIntervention(){
-      this.interventionrecordservice.delete(this.intervention_record.id)
+  }
+  deleteIntervention() {
+    this.interventionrecordservice.delete(this.intervention_record.id)
       .subscribe(
-        data=>{
+        data => {
           console.log(data);
           this.router.navigate(['intervention-record/all']);
         },
-        error=>{
+        error => {
           console.log(error)
         });
-    }
+  }
 
 }
